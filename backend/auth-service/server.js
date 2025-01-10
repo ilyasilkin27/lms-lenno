575f0e0d6ba29a19d6273f1140903cdb06af7e99
ilyasilkin27 <ilyasilkin27@gmail.com>
2025-01-10 22:21:12 +0300

    update server.js

--- backend/auth-service/server.js
+++ backend/auth-service/server.js
@@ -17,7 +17,7 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Auth service running on port ${PORT}`);
 });

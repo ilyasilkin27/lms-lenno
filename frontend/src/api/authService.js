13670444eb1437e06bc8103a1a41d3c8635516bb
ilyasilkin27 <ilyasilkin27@gmail.com>
2025-01-10 18:35:51 +0300

    update authController.js, authService.js and userService.js

--- frontend/src/api/authService.js
+++ frontend/src/api/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const USER_SERVICE_API_URL = 'http://localhost:5001/api/users';
+const USER_SERVICE_API_URL = 'https://lms-lenno-user-service.onrender.com/api/users';
 
 export const registerUser = async (userData) => {
   try {

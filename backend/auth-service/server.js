247f872e3ac4828ad8fc209c948558bef6c86321
ilyasilkin27 <ilyasilkin27@gmail.com>
2025-01-10 20:28:36 +0300

    update 6 files

--- backend/auth-service/server.js
+++ backend/auth-service/server.js
@@ -1,13 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import authRoutes from './routes/authRoutes.js';
 
 dotenv.config();
+
+const corsOptions = {
+  origin: 'https://lms-lenno-frontend.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
 
 const app = express();
 
 app.use(express.json());
 app.use('/api/auth', authRoutes);
+app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
--- backend/user-service/server.js
+++ backend/user-service/server.js
@@ -1,6 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 import userRoutes from './routes/userRoutes.js';
 import errorHandler from './middlewares/errorMiddleware.js';
+
+const corsOptions = {
+  origin: 'https://lms-lenno-frontend.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
 
 const app = express();
 const port = 5001;
@@ -9,6 +16,7 @@ app.use(express.json());
 
 app.use(userRoutes);
 app.use(errorHandler);
+app.use(cors(corsOptions));
 
 app.listen(port, () => {
   console.log(`User service is running on http://localhost:${port}`);

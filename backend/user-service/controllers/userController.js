cfe83d7ab6218dd1f54753fd2a7af07200794932
ilyasilkin27 <ilyasilkin27@gmail.com>
2025-01-14 01:41:32 +0300

    update userController.js

--- backend/user-service/controllers/userController.js
+++ backend/user-service/controllers/userController.js
@@ -26,21 +26,47 @@ const checkExistingUser = async (login, password) => {
   return data;
 };
 
-const createUser = async (name, surname, login, password, role = 'teacher') => {
-  const salt = await bcrypt.genSalt(10);
-  const password_hash = await bcrypt.hash(password, salt);
-  const { data, error } = await supabase
-    .from('users')
-    .insert([{ name, surname, login, password: password_hash, role }])
-    .select()
-    .single();
+const validateInput = (name, surname, password) => {
+  const namePattern = /^[a-zA-Z\-]{3,30}$/;
+  const surnamePattern = /^[a-zA-Z\-]{3,30}$/;
+  const passwordPattern = /^[^\s]{8,}$/;
 
-  if (error) {
-    console.error('Error inserting user:', error);
-    throw new Error('Не удалось создать пользователя');
+  if (!namePattern.test(name)) {
+    throw new Error('Имя должно содержать от 3 до 30 символов, допускать только буквы и "-", и не содержать пробелов.');
   }
 
-  return data;
+  if (!surnamePattern.test(surname)) {
+    throw new Error('Фамилия должна содержать от 3 до 30 символов, допускать только буквы и "-", и не содержать пробелов.');
+  }
+
+  if (!passwordPattern.test(password)) {
+    throw new Error('Пароль должен содержать минимум 8 символов и не содержать пробелов.');
+  }
+};
+
+const createUser = async (name, surname, login, password, role = 'teacher') => {
+  try {
+    validateInput(name, surname, password);
+
+    const salt = await bcrypt.genSalt(10);
+    const password_hash = await bcrypt.hash(password, salt);
+
+    const { data, error } = await supabase
+      .from('users')
+      .insert([{ name, surname, login, password: password_hash, role }])
+      .select()
+      .single();
+
+    if (error) {
+      console.error('Error inserting user:', error);
+      throw new Error('Не удалось создать пользователя');
+    }
+
+    return data;
+  } catch (error) {
+    console.error(error.message);
+    throw error;
+  }
 };
 
 export { checkExistingUser, createUser };
